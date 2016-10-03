3aa7fd3ed4e4d076aca6a2186e0cb52cbe70c4ec
RaquelitaR <raquelita15@ru.is>
2016-10-03 22:14:21 +0000

    Added the API for all three classes

---
+++ s4/WordNet.java
@@ -0,0 +1,45 @@
+package s4;
+
+import edu.princeton.cs.algs4.*;
+import java.util.*;
+
+/**
+ * Created by raquelitarosaguilar on 03/10/2016.
+ */
+public class WordNet {
+
+    // constructor takes the name of the two input files
+    public WordNet(String synsets, String hypernyms){
+
+    }
+
+    // return all WordNet nouns
+    public Iterable<String> nouns();
+
+    // is the word a WordNet nound?
+    public boolean isNoun(String word){
+        return ;
+    }
+
+    // distance between nounA and nounB (defined below)
+    public int distance(String nounA, String nounB){
+        return ;
+    }
+
+    // a synset (second field of synsets.txt) that is the common
+    // ancestor of nounA and nounB
+    public String sap(String nounA, String nounB){
+        return :
+    }
+
+    /**
+     * Test client
+     * Do a unit testing of this class
+     */
+    public static void main(String[] args){
+
+    }
+
+
+
+}
