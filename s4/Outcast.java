51264bd56f0e34e3569032a9fd031f337f473243
RaquelitaR <raquelita15@ru.is>
2016-10-03 22:15:47 +0000

    Added the API for all three classes

---
+++ s4/Outcast.java
@@ -0,0 +1,20 @@
+package s4;
+
+import edu.princeton.cs.algs4.*;
+import java.util.*;
+
+/**
+ * Created by raquelitarosaguilar on 03/10/2016.
+ */
+public class Outcast {
+
+    // constructor takes a WordNet object
+    public Outcast(WordNet wordNet){
+        return;
+    }
+
+    // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns){
+
+    }
+}
---
+++ s4/SAP.java
@@ -0,0 +1,47 @@
+package s4;
+
+import edu.princeton.cs.algs4.*;
+import java.util.*;
+
+/**
+ * Created by raquelitarosaguilar on 03/10/2016.
+ */
+public class SAP {
+
+    // constructor takes a digraph (not necessarily a DAG)
+    public SAP(Diagraph G){
+
+    }
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w){
+        return;
+    }
+
+    // a common ancestor of v and w that participates in a shortest ancestral path
+    // -1 if no such path
+    public int ancesto(int v, int w){
+        return;
+    }
+
+    // length of shortest ancestral path between any vertex in v and any vertex in w
+    // -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w){
+        return;
+    }
+
+    // a common ancestor that participates in shortest ancestral path
+    // -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
+        return;
+    }
+
+    /**
+     * Test client
+     * Do a unit testing of this class
+     */
+    public static void main(String[] args){
+
+    }
+
+}
