2bd28ac6767ec0f29f1f2e9a1b193a2d0352a43c
Ottriman <gogo.gogo520@gmail.com>
2016-10-13 17:28:44 +0000

    Mest af Outcast
    Þarf kanski að fikta í word net sem er gert í main þannig það noti file
    eins og td outcast9.
    
    Ætti að virka annars með completed word tree.

--- s4/src/Outcast.java
+++ s4/src/Outcast.java
@@ -18,6 +18,32 @@ public class Outcast {
     // given an array of WordNet nouns, return an outcast
     public String outcast(String[] nouns){
         String outcast = null;
+        int totalDistance = 0; //Save value for adding up all the distances
+        int outcastDistance = 0; //Distance of current outcast
+        
+        for(int i = 0; i < nouns.length; i++)
+        {
+        	// Check distance to every point from nouns[i] and add it all together?
+        	// Compare that to outcast and if greater, make outcast be that string.
+        	
+        	if(outcast == null) // a first case just to make it not null and enable comparisons
+        	{
+        		outcast = nouns[0];
+        	}
+        	
+        	for(int k = 0; k < nouns.length; k++)
+        	{
+        		totalDistance += wordNet.distance(nouns[i], nouns[k]);
+        	}
+        	
+        	if(outcastDistance > totalDistance)
+        	{
+        		outcastDistance = totalDistance;
+        		outcast = nouns[i];
+        	}
+        	
+        	totalDistance = 0; // reset to 0.
+        }
 
         return outcast;
 
